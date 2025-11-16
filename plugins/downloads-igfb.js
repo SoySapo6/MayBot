a2503c20bca974895d6f979582cafc1b73701958
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-11-16 17:43:35 -0400

    Update downloads-igfb.js

--- plugins/downloads-igfb.js
+++ plugins/downloads-igfb.js
@@ -1,27 +1,48 @@
-const handler = async (m, { args, conn, usedPrefix }) => {
+const handler = async (m, { args, conn, usedPrefix, command }) => {
 try {
-if (!args[0]) return conn.reply(m.chat, `❀ Por favor, ingresa un enlace de Instagram/Facebook.`, m)
+if (!args[0]) return conn.reply(m.chat, `❀ Por favor, ingresa un enlace de *Instagram* o *Facebook*.`, m)
 let data = []
-try {
+const url = encodeURIComponent(args[0])
 await m.react('🕒')
-const api = `${global.APIs.vreden.url}/api/igdownload?url=${encodeURIComponent(args[0])}`
-const res = await fetch(api)
-const json = await res.json()
-if (json.resultado?.respuesta?.datos?.length) {
-data = json.resultado.respuesta.datos.map(v => v.url)
-}} catch {}
-if (!data.length) {
+if (/(instagram\.com)/i.test(args[0])) {
 try {
-const api = `${global.APIs.delirius.url}/download/instagram?url=${encodeURIComponent(args[0])}`
+const api = `${global.APIs.adonix.url}/download/instagram?apikey=${global.APIs.adonix.key}&url=${url}`
 const res = await fetch(api)
 const json = await res.json()
 if (json.status && json.data?.length) {
 data = json.data.map(v => v.url)
-}} catch {}
+}} catch (e) {}
+}
+if (/(facebook\.com|fb\.watch)/i.test(args[0]) && !data.length) {
+try {
+const api = `${global.APIs.adonix.url}/download/facebook?apikey=${global.APIs.adonix.key}&url=${url}`
+const res = await fetch(api)
+const json = await res.json()
+if (json.status && json.result?.media?.video_hd) {
+data = [json.result.media.video_hd]
+}} catch (e) {}
+}
+if (!data.length) {
+try {
+const api = `${global.APIs.vreden.url}/api/igdownload?url=${url}`
+const res = await fetch(api)
+const json = await res.json()
+if (json.resultado?.respuesta?.datos?.length) {
+data = json.resultado.respuesta.datos.map(v => v.url)
+}} catch (e) {}
+}
+if (!data.length) {
+try {
+const api = `${global.APIs.delirius.url}/download/instagram?url=${url}`
+const res = await fetch(api)
+const json = await res.json()
+if (json.status && json.data?.length) {
+data = json.data.map(v => v.url)
+}} catch (e) {}
 }
 if (!data.length) return conn.reply(m.chat, `ꕥ No se pudo obtener el contenido.`, m)
 for (let media of data) {
-await conn.sendFile(m.chat, media, 'instagram.mp4', `❀ Aquí tienes ฅ^•ﻌ•^ฅ.`, m)
+await conn.sendFile(m.chat, media, 'media.mp4', `❀ Aquí tienes ฅ^•ﻌ•^ฅ.`, m)
 await m.react('✔️')
 }} catch (error) {
 await m.react('✖️')
